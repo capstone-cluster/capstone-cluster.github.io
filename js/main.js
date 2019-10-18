a6a8e05e4af24f63b8101d7c034ab30630521d41
Theo Fleck <emailtfleck@gmail.com>
2019-10-18 13:21:28 -0400

    remove album folder for images

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 //Populate heavy items after DOM loaded
 $(window).on("load", function() {
-  $('#album1').attr('src', 'img/album/theo_fleck.jpg');
-  $('#album2').attr('src', 'img/album/adam_dadey.jpg');
-  $('#album3').attr('src', 'img/album/theo_fleck.jpg');
-  $('#album4').attr('src', 'img/album/theo_fleck.jpg');
+  $('#album1').attr('src', 'img/theo_fleck.jpg');
+  $('#album2').attr('src', 'img/adam_dadey.jpg');
+  $('#album3').attr('src', 'img/theo_fleck.jpg');
+  $('#album4').attr('src', 'img/theo_fleck.jpg');
 });
 //Shrink navbar on scroll
 $(window).scroll(function() {
